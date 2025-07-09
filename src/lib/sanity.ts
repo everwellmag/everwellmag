7c76a6a0637c74203f2055ec6a732600c8d4efe5
MinhDai <everwellmag@gmail.com>
2025-07-09 15:00:23 +0700

    Add .env.local with projectId 6g0woys7 and dataset production, fix line endings with .gitattributes

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,5 +4,5 @@ export const sanity = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   useCdn: true,
-  apiVersion: '2023-01-01',
+  apiVersion: '2025-07-09', // Cập nhật API version
 })

cfe477b9b200e4c1da050aa76565b000b4189fa7
MinhDai <everwellmag@gmail.com>
2025-08-19 11:36:17 +0700

    Update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+// postcss.config.mjs
 
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};

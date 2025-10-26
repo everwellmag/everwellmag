853a356d6e82e5962cdb74da0b3bd9c316d423c2
MinhDai <everwellmag@gmail.com>
2025-10-26 16:12:52 +0700

    fixcomment only local

--- src/components/content/comments/comment-form.tsx
+++ src/components/content/comments/comment-form.tsx
@@ -46,7 +46,7 @@ export default function CommentForm({ articleSlug, productSlug }: CommentFormPro
                 throw new Error(`${entityType} not found`);
             }
 
-            const response = await fetch('http://localhost:3000/api/comments', {
+            const response = await fetch('/api/comments', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({

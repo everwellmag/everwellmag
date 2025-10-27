b46a0c8523b95136d8c6d6deb4d06e2fc25085a5
MinhDai <everwellmag@gmail.com>
2025-10-27 11:43:29 +0700

    Redirect everwellmag to everwellmagazine canonical

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // src/lib/config.ts
-export const SITE_DOMAIN = 'https://www.everwellmag.com';
+export const SITE_DOMAIN = 'https://www.everwellmagazine.com';
 export const CMS_DOMAIN = 'https://cms.everwellmag.com';
 export const DEFAULT_OG_IMAGE = `${CMS_DOMAIN}/uploads/default_image_0295f000e6.jpg`;
